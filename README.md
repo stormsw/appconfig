b513e021677e672bed70af20c1e9b65de1de2639
Oleksander Varchenko <ovarchenko@ils.com.ua>
2013-09-10 12:51:45 +0300

    Added Thor command lineprocessor

--- README.md
+++ README.md
@@ -62,6 +62,37 @@ Indent every line of the block by at least 4 spaces or 1 tab. Alternatively, you
  
 ``` 
 Text to appear as a code block.
+
+filename = 'app.config'
+
+doc = Nokogiri::XML(File.open(filename)) do |config|
+# NOBLANKS - Remove blank nodes
+# NOENT - Substitute entities
+# NOERROR - Suppress error reports
+# STRICT - Strict parsing; raise an error when parsing malformed documents
+# NONET - Prevent any network connections during parsing. Recommended for parsing untrusted documents.
+  config.strict.nonet
+end
+
+#print doc.to_s
+#wizards = doc.css("Wizards wizard")
+wizards = doc.xpath('//wizard')
+puts "Found #{wizards.count}'s wizards sections document wide."
+
+editors_names_usage = Hash.new { |h, k| h[k] = 0 }
+editors = doc.xpath('//editor')
+
+puts "Found #{editors.count}'s editors sections document wide."
+
+editors.each do |editor|
+#  puts editor.attributes.inspect
+#  exit
+  clear_name = editor[:name].split(':').first
+  editors_names_usage[clear_name] += 1
+end
+
+puts editors_names_usage.inspect
+
 ``` 
  
 Within a code block, ampersands (&) and angle brackets (< and >)are automatically converted into HTML entities.
--- appconfig.rb
+++ appconfig.rb
@@ -1,31 +1,48 @@
 require 'nokogiri'
+require 'thor'
 
-filename = 'app.config'
 
-doc = Nokogiri::XML(File.open(filename)) do |config|
-# NOBLANKS - Remove blank nodes
-# NOENT - Substitute entities
-# NOERROR - Suppress error reports
-# STRICT - Strict parsing; raise an error when parsing malformed documents
-# NONET - Prevent any network connections during parsing. Recommended for parsing untrusted documents.
-  config.strict.nonet
+module AppConfigCLI
+	class Appconfig < Thor
+		
+		desc "info FileName", "Configuration Info of FileName"
+		method_option	:filename, 								
+								:default => "app.config", 
+								:aliases => "-i",
+								:desc => "Configuration file path"
+		def info(filename)
+			doc = Nokogiri::XML(File.open(filename)) do |config|
+			# NOBLANKS - Remove blank nodes
+			# NOENT - Substitute entities
+			# NOERROR - Suppress error reports
+			# STRICT - Strict parsing; raise an error when parsing malformed documents
+			# NONET - Prevent any network connections during parsing. Recommended for parsing untrusted documents.
+			  config.strict.nonet
+			end
+
+			#print doc.to_s
+			#wizards = doc.css("Wizards wizard")
+			wizards = doc.xpath('//wizard')
+			puts "Found #{wizards.count}'s wizards sections document wide."
+
+			editors_names_usage = Hash.new { |h, k| h[k] = 0 }
+			editors = doc.xpath('//editor')
+
+			puts "Found #{editors.count}'s editors sections document wide."
+
+			editors.each do |editor|
+			#  puts editor.attributes.inspect
+			#  exit
+			  clear_name = editor[:name].split(':').first
+			  editors_names_usage[clear_name] += 1
+			end
+
+			puts editors_names_usage.inspect
+			
+		end 
+#		desc "remote SUBCOMMAND ...ARGS", "manage set of tracked repositories"
+#		subcommand "remote", Remote	
+	end
 end
 
-#print doc.to_s
-#wizards = doc.css("Wizards wizard")
-wizards = doc.xpath('//wizard')
-puts "Found #{wizards.count}'s wizards sections document wide."
-
-editors_names_usage = Hash.new { |h, k| h[k] = 0 }
-editors = doc.xpath('//editor')
-
-puts "Found #{editors.count}'s editors sections document wide."
-
-editors.each do |editor|
-#  puts editor.attributes.inspect
-#  exit
-  clear_name = editor[:name].split(':').first
-  editors_names_usage[clear_name] += 1
-end
-
-puts editors_names_usage.inspect
+AppConfigCLI::Appconfig.start
